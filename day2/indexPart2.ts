e4f84ac6aa6bb0ba1acfed808e3facf50a66180f
steveneng <steven.e11373@gmail.com>
2021-12-02 13:27:00 -0500

    day 2 part 2 finished

--- day2/indexPart2.ts
+++ day2/indexPart2.ts
@@ -1,22 +1,25 @@
 import fs from "fs";
 
-let listOfDepths:string[] = fs.readFileSync(`${__dirname}\\test.txt`,"utf-8").split("\n")
+let listOfDepths:string[] = fs.readFileSync(`${__dirname}\\input.txt`,"utf-8").split("\n")
 
-let netY = 0; 
 let netX = 0;
+let aim = 0; 
+let depth = 0
 
 for(let i = 0 ; i<listOfDepths.length ; i++){
     let currentDirection:string[] = listOfDepths[i].split(" ");
     let direction:number = parseInt(currentDirection[1])
     if(currentDirection[0]==="forward"){
         netX +=direction
+        depth +=aim*direction 
     }
     else if(currentDirection[0]==="down"){
-        netY +=direction
+        aim +=direction
     }
     else if(currentDirection[0]==="up"){
-        netY -=direction
+        aim -=direction
     }
 }
 
-console.log(netX*netY)
+console.log(depth*netX)
+
