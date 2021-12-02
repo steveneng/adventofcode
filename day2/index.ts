2ae88b442827c70d144e1478f982fd9293ba31e7
steveneng <steven.e11373@gmail.com>
2021-12-02 11:54:36 -0500

    added in types

--- day2/index.ts
+++ day2/index.ts
@@ -1,13 +1,13 @@
 import fs from "fs";
 
-let listOfDepths = fs.readFileSync(`${__dirname}\\input.txt`,"utf-8").split("\n")
+let listOfDepths:string[] = fs.readFileSync(`${__dirname}\\input.txt`,"utf-8").split("\n")
 
 let netY = 0; 
 let netX = 0;
 
 for(let i = 0 ; i<listOfDepths.length ; i++){
-    let currentDirection = listOfDepths[i].split(" ");
-    let direction = parseInt(currentDirection[1])
+    let currentDirection:string[] = listOfDepths[i].split(" ");
+    let direction:number = parseInt(currentDirection[1])
     if(currentDirection[0]==="forward"){
         netX +=direction
     }
