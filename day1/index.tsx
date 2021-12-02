4be054ecd81f7eb064f2cfb87bb12c0d777de9b1
steveneng <steven.e11373@gmail.com>
2021-12-02 10:32:28 -0500

    day1 challenge completed

---
+++ day1/index.tsx
@@ -0,0 +1,18 @@
+const fs = require('fs')
+
+let ele:string = fs.readFileSync(__dirname+'/input2.txt','utf8',)
+let inputList:string[] = ele.split("\n")
+
+let current = inputList[0];
+let count = 0
+
+for(let i = 1 ; i<inputList.length; i++){
+    if(inputList[i]>current){
+        count++
+        current=inputList[i]
+    }
+    else{
+        current = inputList[i]
+    }
+}
+
